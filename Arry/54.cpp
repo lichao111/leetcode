81892b6cc622a3fb40a40ab1d97dbad101c933a0
Chao <515359166@qq.com>
2018-08-11 16:40:13 +0800

    54:Spiral Matrix

--- Arry/54.cpp
+++ Arry/54.cpp
@@ -12,14 +12,50 @@ Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    vector<int> spiralOrder(vector<vector<int> >& matrix) {
+        vector<int> t_nRet;
+        if(matrix.empty()) return t_nRet;
         int column= matrix[0].size();
         int row = matrix.size();
-        vector<int> t_nRet;
-        for(int i = 0,j= 0;)
+        int t_niter = column*row;
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while(true)
         {
-
+            while(j<column)
+            {
+                t_nRet.push_back(matrix[i][j++]);
+                if(t_nRet.size()==t_niter) return t_nRet;
+            }
+            j--;
+            i++;
+            while(i<row)
+            {
+                t_nRet.push_back(matrix[i++][j]);
+                if(t_nRet.size()==t_niter) return t_nRet;
+            }
+            i--;
+            j--;
+            while(j>=k)
+            {
+                t_nRet.push_back(matrix[i][j--]);
+                if(t_nRet.size()==t_niter) return t_nRet;
+            }
+            j++;
+            i--;
+            while(i>k)
+            {
+                t_nRet.push_back(matrix[i--][j]);
+                if(t_nRet.size()==t_niter) return t_nRet;
+            }
+            i++;
+            j++;
+            k++;
+            row--;
+            column--;
+            //Display(t_nRet);
         }
-
+        return t_nRet;
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 
-#include "./Arry/53.cpp"
+#include "./Arry/54.cpp"
 
 #include "./Arry/BinarySearch.cpp"
 
@@ -7,8 +7,23 @@ int main()
 {
 	Solution t_mSoluton;
 	//5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5
-	int myints1[] = {-1,-1,-1,-1,-1,100,-100};
-	std::vector<int> nums1(myints1, myints1 + sizeof(myints1) / sizeof(int));
+	int myints11[] = {1,2,3,4};
+	std::vector<int> nums11(myints11, myints11 + sizeof(myints11) / sizeof(int));
+
+	int myints12[] = {5,6,7,8};
+	std::vector<int> nums12(myints12, myints12 + sizeof(myints12) / sizeof(int));
+
+	int myints13[] = {9,10,11,12};
+	std::vector<int> nums13(myints13, myints13 + sizeof(myints13) / sizeof(int));
+
+	int myints14[] = {13,14,15,16};
+	std::vector<int> nums14(myints14, myints14 + sizeof(myints14) / sizeof(int));
+
+	vector<vector<int> > nums1;
+	// nums1.push_back(nums11);
+	// nums1.push_back(nums12);
+	// nums1.push_back(nums13);
+	//nums1.push_back(nums14);
 
 	int myints2[] = { 10, 1, 2, 7, 6, 1, 5 };
 	std::vector<int> nums2(myints2, myints2	 + sizeof(myints2) / sizeof(int));
@@ -23,7 +38,7 @@ int main()
 	//DisplayLiseNode(l2);
 	//DisplayLiseNode(l3);
 
-	cout<<t_mSoluton.maxSubArray(nums1)<<endl;
+	Display(t_mSoluton.spiralOrder(nums1));
 	
 	return 0;
 }
