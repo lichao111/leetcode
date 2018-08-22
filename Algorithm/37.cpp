5d50d8d325d99a5a9659d381fc4ffe3e38b318d0
Chao <515359166@qq.com>
2018-08-22 23:52:25 +0800

    part of 37

---
+++ Algorithm/37.cpp
@@ -0,0 +1,22 @@
+#include "../base.h"
+
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        
+    }
+
+private:
+    bool iscorrect(vector<vector<char> > &board){
+        vector<char> tmp;
+        //row
+        for(int i = 0;i<9;i++){
+            tmp = board[i];
+            
+        }
+        //column
+
+        //3*3
+    }
+
+};
