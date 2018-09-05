9f3008a40df96ed61e1e4907122ee2b874f5044f
Chao <515359166@qq.com>
2018-09-05 21:34:46 +0800

    dele

---
+++ String/889.cpp
@@ -0,0 +1,23 @@
+#include "../base.h"
+
+/*
+
+Input: Input: S = S = "cba""cba", K =1
+ Output: Output: "acb""acb"
+ Explanation: Explanatio 
+In the first move, we move the 1st character ("c") to the end, obtaining the string "bac".
+In the second move, we move the 1st character ("b") to the end, obtaining the final result "acb".
+Example 2:
+
+Input: S = "baaca", K = 3
+Output: "aaabc"
+Explanation: 
+In the first move, we move the 1st character ("b") to the end, obtaining the string "aacab".
+In the second move, we move the 3rd character ("c") to the end, obtaining the final result "aaabc".*/
+
+class Solution {
+public:
+    string orderlyQueue(string S, int K) {
+        
+    }
+};
